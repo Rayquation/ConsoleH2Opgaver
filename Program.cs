ec664bfbeb033eee1e593fa40b6584db23c42c93
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2023-02-21 09:13:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleH2Opgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 2;
+            Add2(ref amount);
+            Console.WriteLine($"Nuværende værdi af amount er : {amount}");
+            Console.ReadKey();
+        }
+        static void Add2(ref int a)
+        {
+            a += 2;
+        }
+    }
+}
